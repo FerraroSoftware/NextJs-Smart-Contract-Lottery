16ac9b5f46e8bd6abb2ff3e79ca9952cc0ec6e86
FerraroSoftware <software.ferraro@gmail.com>
2022-06-10 18:24:06 -0400

    runcontractfunction getentrancefee working

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,16 +1,50 @@
 // function to call lottery
 // moralis has hook useWeb3Contract
 import { useWeb3Contract } from "react-moralis";
-import { abi, contractAddress } from "../constants";
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 export default function LotteryEntrance() {
-    const {runContractFunction: enterRaffle} = useWeb3Contract({
-       abi: ,
-       contractAddress: ,
-       functionName: ,
-       params: {},
-       msgValue:
-    })
+  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const chainId = parseInt(chainIdHex);
+  const raffleAddress =
+    chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+  // starting value 0
+  const [entranceFee, setEntranceFee] = useState("0");
 
-  return <div>lottery entrance</div>;
+  // const {runContractFunction: enterRaffle} = useWeb3Contract({
+  //    abi: abi,
+  //    contractAddress: raffleAddress,
+  //    functionName: "enterRaffle",
+  //    params: {},
+  //    msgValue:
+  // })
+
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      // try to read raffle entrance fee
+      // if using await, need to put in async function to call it correctly
+      async function updateUI() {
+        const entranceFeeFromCall = (await getEntranceFee()).toString();
+        setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall, "ether"));
+      }
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+
+  return (
+    <div>
+      lottery entrance
+      <div>Entrance Fee: {entranceFee}</div>
+    </div>
+  );
 }
