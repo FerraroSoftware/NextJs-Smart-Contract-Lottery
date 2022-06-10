4d750974c8064f97bb3acaca0c802151183c0b8e
FerraroSoftware <software.ferraro@gmail.com>
2022-06-10 19:10:34 -0400

    isfetching or isloading button update spin

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,11 @@ import { ConnectButton } from "web3uikit";
 
 export default function Header() {
   return (
-    <div>
+    <div className="p-5 border-b-2 flex flex-row">
       {/* not connecting to server  */}
       {/* This does everything manualheader does -.- */}
-      Raffle
+      <h1 className="py-4 px-4 font-blog text-3xl">Raffle</h1>
+      <div className="ml-auto py-2 px-4"></div>
       <ConnectButton moralisAuth={false}></ConnectButton>
     </div>
   );
--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -20,7 +20,11 @@ export default function LotteryEntrance() {
   // used for pop ups
   const dispatch = useNotification();
 
-  const { runContractFunction: enterRaffle } = useWeb3Contract({
+  const {
+    runContractFunction: enterRaffle,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
     abi: abi,
     contractAddress: raffleAddress,
     functionName: "enterRaffle",
@@ -83,24 +87,32 @@ export default function LotteryEntrance() {
   };
 
   return (
-    <div>
+    <div className="p-5">
       lottery entrance
       <div>
         {raffleAddress ? (
           <div>
             <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
               onClick={async function () {
                 await enterRaffle({
                   onSuccess: handleSuccess,
                   onError: (error) => console.log(error),
                 });
               }}
+              disabled={isLoading || isFetching}
             >
-              Enter Raffle
+              {isLoading || isFetching ? (
+                <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+              ) : (
+                <div>Enter Raffle</div>
+              )}
             </button>
-            Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH{" "}
-            Players: {numPlayers}
-            Recent Winner: {recentWinner}
+            <div>
+              Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH{" "}
+            </div>
+            <div>Players: {numPlayers}</div>
+            <div>Recent Winner: {recentWinner}</div>
           </div>
         ) : (
           <div>No Raffle Address Detected</div>
